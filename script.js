6e1a199ce343e53de13a7caeef171494c194ea82
Kavin L <142073658+kavinlakshmanan@users.noreply.github.com>
2024-04-18 22:31:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+  }
+function openInNewTab(url) {
+    window.open(url, '_blank');
+  }
